ea8b3f8735f9a8d25df3b9b9d34fbc89b743a34e
Daniel Laloush <d.laloush@outlook.com>
2020-05-16 18:02:58 -0400

    Updates

---
+++ precache-manifest.9fe516c61fbd5c2fcb8298092efff169.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333a407d06c66e4e9e6982b8d355033d",
+    "url": "/ndm-test-repo/index.html"
+  },
+  {
+    "revision": "5b74c36c1230cd51fa23",
+    "url": "/ndm-test-repo/static/css/main.705d220b.chunk.css"
+  },
+  {
+    "revision": "40dda88a66e14f78e777",
+    "url": "/ndm-test-repo/static/js/2.9f1f32a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ndm-test-repo/static/js/2.9f1f32a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b74c36c1230cd51fa23",
+    "url": "/ndm-test-repo/static/js/main.5e110ce3.chunk.js"
+  },
+  {
+    "revision": "310ef9c76d73f1d86ed0",
+    "url": "/ndm-test-repo/static/js/runtime-main.41cdcda8.js"
+  },
+  {
+    "revision": "e29266125d4df3c616ea721682254f2d",
+    "url": "/ndm-test-repo/static/media/GildaDisplay-Regular.e2926612.ttf"
+  },
+  {
+    "revision": "0d5f74db34aab37da60d2e18e443d2e1",
+    "url": "/ndm-test-repo/static/media/SuezOne-Regular.0d5f74db.ttf"
+  },
+  {
+    "revision": "231c34bb1d125bd84db6640559379df2",
+    "url": "/ndm-test-repo/static/media/list-type.231c34bb.svg"
+  },
+  {
+    "revision": "d1cbca6ce00538015de476b597ee0b2c",
+    "url": "/ndm-test-repo/static/media/logo.d1cbca6c.png"
+  },
+  {
+    "revision": "ce7b6fde8c789b84b7e9876a6cbe75a0",
+    "url": "/ndm-test-repo/static/media/panama1.ce7b6fde.jpg"
+  },
+  {
+    "revision": "2444d6b0b9f5ea7433d81cb4edabcb72",
+    "url": "/ndm-test-repo/static/media/panama2.2444d6b0.jpg"
+  },
+  {
+    "revision": "ff15a53ef9884fac0f5773b85fa83720",
+    "url": "/ndm-test-repo/static/media/panama3.ff15a53e.jpg"
+  }
+]);
